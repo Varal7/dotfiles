8a70a57b441e3b5c459a189454479038da09c9b1
Victor Quach <varaal@gmail.com>
2020-09-20 20:11:17 -0400

    Install zsh extensions

--- install.sh
+++ install.sh
@@ -41,3 +41,17 @@ fi
 # Install vim plugins
 
 vim -c 'PlugInstall' -c 'qa!'
+
+# Install zsh extensions
+
+## Spaceship
+
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# Zsh syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Zsh completions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+
