65fe9da7deaba67aca38748e2cab487ca6de9685
Victor Quach <varaal@gmail.com>
2020-09-20 20:18:07 -0400

    Path

--- install.sh
+++ install.sh
@@ -46,8 +46,8 @@ vim -c 'PlugInstall' -c 'qa!'
 
 ## Spaceship
 
-git clone https://github.com/denysdovhan/spaceship-prompt.git ${ZSH_CUSTOM:-/.oh-my-zsh/custom}/themes/spaceship-prompt --depth=2
-ln -s ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH_CUSTOM:-/.oh-my-zsh/custom}/themes/spaceship.zsh-theme
+git clone https://github.com/denysdovhan/spaceship-prompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt --depth=2
+ln -s ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship.zsh-theme
 
 # Zsh syntax highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
