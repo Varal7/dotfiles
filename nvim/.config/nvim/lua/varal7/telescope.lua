7129400f20458f933cc735b3ce865aab489b287e
Victor Quach <varaal@gmail.com>
2022-04-04 11:52:15 -0400

    Skhd

--- nvim/.config/nvim/lua/varal7/telescope.lua
+++ nvim/.config/nvim/lua/varal7/telescope.lua
@@ -31,3 +31,14 @@ require("telescope").setup({
 })
 
 require("telescope").load_extension("fzy_native")
+
+local M = {}
+
+M.project_files = function()
+  local opts = {} -- define here if you want to define something
+  local ok = pcall(require"telescope.builtin".git_files, opts)
+  if not ok then require"telescope.builtin".find_files({ hidden = true}) end
+end
+
+return M
+
