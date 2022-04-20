047a0800d24bce3dc1d441701de2c7ea4a1554ac
Victor Quach <varaal@gmail.com>
2022-04-20 12:02:59 -0400

    No need for lots of spaceship plugins

--- zsh/.config/zsh/config.d/spaceship.zsh
+++ zsh/.config/zsh/config.d/spaceship.zsh
@@ -8,26 +8,26 @@ export SPACESHIP_PROMPT_ORDER=(
     hg            # Mercurial section (hg_branch  + hg_status)
     package       # Package version
     node          # Node.js section
-    ruby          # Ruby section
-    elm           # Elm section
-    elixir        # Elixir section
+    # ruby          # Ruby section
+    # elm           # Elm section
+    # elixir        # Elixir section
     xcode         # Xcode section
     swift         # Swift section
-    golang        # Go section
-    php           # PHP section
-    rust          # Rust section
-    haskell       # Haskell Stack section
-    julia         # Julia section
-    docker        # Docker section
-    aws           # Amazon Web Services section
-    gcloud        # Google Cloud Platform section
+    # golang        # Go section
+    # php           # PHP section
+    # rust          # Rust section
+    # haskell       # Haskell Stack section
+    # julia         # Julia section
+    # docker        # Docker section
+    # aws           # Amazon Web Services section
+    # gcloud        # Google Cloud Platform section
     venv          # virtualenv section
     conda         # conda virtualenv section
     pyenv         # Pyenv section
-    dotnet        # .NET section
-    ember         # Ember.js section
-    kubectl       # Kubectl context section
-    terraform     # Terraform workspace section
+    # dotnet        # .NET section
+    # ember         # Ember.js section
+    # kubectl       # Kubectl context section
+    # terraform     # Terraform workspace section
     git_last_commit
     proxy
     exec_time     # Execution time
